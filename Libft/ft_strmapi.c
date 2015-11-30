56a600ebc5ed7da20adcc99fb4b40067ff32d77f
Eddy ALBERT <ealbert@e1r9p14.42.fr>
2015-11-30 13:04:19 +0100

    Removed

---
+++ Libft/ft_strmapi.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ealbert <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/11/30 12:01:26 by ealbert           #+#    #+#             */
+/*   Updated: 2015/11/30 12:40:51 by ealbert          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(const char *s, char (*f)(unsigned int, char))
+{
+	unsigned int 		i;
+	char				*new;
+
+	i = 0;
+	if (s && f)
+		new = malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (!new)
+		return (NULL);
+	while (s[i])
+	{
+		new[i] = f(i, s[i]);
+		i++;
+	}
+	new[i] = '\0';
+	return (0);
+}
