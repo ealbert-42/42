1bc12496371f40281a5642bef15c7474152c4fd2
Eddy ALBERT <ealbert@e1r2p11.42.fr>
2016-01-18 14:25:29 +0100

    Final Libft

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: ealbert <ealbert@student42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/23 11:19:11 by ealbert           #+#    #+#             */
-/*   Updated: 2015/12/03 16:21:38 by ealbert          ###   ########.fr       */
+/*   Updated: 2016/01/18 11:38:00 by ealbert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 char	*ft_strncpy(char *dst, const char *src, size_t n)
 {
 	char	*s;
-	
+
 	s = dst;
 	while (n > 0 && *src != '\0')
 	{
